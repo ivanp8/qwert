0fcea45001955a316131d5e19d6e6feadb854fef
Pshenyk <IPshenyk@luxoft.com>
2022-03-13 14:34:34 +0200

    Main Commit

--- qwert/qwert.cpp
+++ qwert/qwert.cpp
@@ -11,7 +11,6 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
-// Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
